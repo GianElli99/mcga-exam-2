20f06fba06db4f617a28e13266c058d00671d024
Gianfranco Elli <gianelli99@hotmail.com>
2021-12-08 19:40:33 -0300

    some css modified

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -5,6 +5,7 @@ import styles from './NavBar.module.css';
 export const NavBar = () => {
   return (
     <nav className={styles.navBar}>
+      <h1>Easy Buy</h1>
       <ul className={styles.list}>
         <li>
           <Link to="..">Dashboard</Link>
--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -2,7 +2,7 @@ import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   palette: {
-    primary: { main: '#ffd740' },
+    primary: { main: '#79B4B7' },
     secondary: { main: '#102541' },
   },
   components: {
