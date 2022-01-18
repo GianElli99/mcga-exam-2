d280fbe46fb172045e6be1625c1600638f76eea0
Gianfranco Elli <gianelli99@hotmail.com>
2022-01-18 18:07:19 -0300

    asd

--- src/components/auth/RequireAuth.jsx
+++ src/components/auth/RequireAuth.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-
+// Comend
 export const RequireAuth = () => {
   const isLogged = useSelector((state) => state.auth.isLogged);
   return isLogged === true ? <Outlet /> : <Navigate to="/login" />;
